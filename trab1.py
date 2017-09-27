43a5e91e16dc9199da28847d57cc89bceafbb7f6
thiagouft <thiagoufma@gmail.com>
2017-09-27 20:37:15 -0300

    Adicionei os print nas transiçoes

--- trab1.py
+++ trab1.py
@@ -1,3 +1,5 @@
+﻿#Aluno: Thiago Silva Pereira
+
 def transicao_estendida(estado, valor):
     #print(valor)
     if valor == '&':
@@ -10,99 +12,130 @@ def transicao_estendida(estado, valor):
 def trasicao_afd(estado, palavra):
     if estado == 'q0':
         if (palavra == 'a'):
-            print("q0 entrou \"" + palavra + "\"")
+            print("q0 entrou \"" + palavra + "\"" + " vai para q2.")
             return 'q2'
         else:
+            print("q0 entrou \"" + palavra + "\"" + " vai para q1.")
             return 'q1'
 
     if estado == 'q1':
         if (palavra == 'a'):
+            print("q1 entrou \"" + palavra + "\"" + " vai para q4.")
             return 'q4'
         else:
+            print("q1 entrou \"" + palavra + "\"" + " vai para q3.")
             return 'q3'
 
     if estado == 'q2':
         if (palavra == 'a'):
+            print("q2 entrou \"" + palavra + "\"" + " vai para q6.")
             return 'q6'
         else:
+            print("q2 entrou \"" + palavra + "\"" + " vai para q5.")
             return 'q5'
 
     if estado == 'q3':
         if (palavra == 'a'):
+            print("q3 entrou \"" + palavra + "\"" + " vai para q8.")
             return 'q8'
         else:
+            print("q3 entrou \"" + palavra + "\"" + " vai para q7.")
             return 'q7'
 
     if estado == 'q4':
         if (palavra == 'a'):
+            print("q4 entrou \"" + palavra + "\"" + " vai para q8.")
             return 'q8'
         else:
+            print("q4 entrou \"" + palavra + "\"" + " vai para q9.")
             return 'q9'
 
     if estado == 'q5':
         if (palavra == 'a'):
+            print("q5 entrou \"" + palavra + "\"" + " vai para q8.")
             return 'q8'
         else:
+            print("q5 entrou \"" + palavra + "\"" + " vai para q10.")
             return 'q10'
 
     if estado == 'q6':
         if (palavra == 'a'):
+            print("q6 entrou \"" + palavra + "\"" + " vai para q8.")
             return 'q8'
         else:
+            print("q6 entrou \"" + palavra + "\"" + " vai para q11.")
             return 'q11'
 
     if estado == 'q7':
         if (palavra == 'a'):
+            print("q7 entrou \"" + palavra + "\"" + " vai para q12.")
             return 'q12'
         else:
+            print("q7 entrou \"" + palavra + "\"" + " vai para q7.")
             return 'q7'
 
     if estado == 'q8':
         if (palavra == 'a'):
+            print("q8 entrou \"" + palavra + "\"" + " vai para q8.")
             return 'q8'
         else:
+            print("q8 entrou \"" + palavra + "\"" + " vai para q8.")
             return 'q8'
 
     if estado == 'q9':
         if (palavra == 'a'):
+            print("q9 entrou \"" + palavra + "\"" + " vai para q13.")
             return 'q13'
         else:
+            print("q9 entrou \"" + palavra + "\"" + " vai para q10.")
             return 'q10'
 
     if estado == 'q10':
         if (palavra == 'a'):
+            print("q10 entrou \"" + palavra + "\"" + " vai para q12.")
             return 'q12'
         else:
+            print("q10 entrou \"" + palavra + "\"" + " vai para q7.")
             return 'q7'
 
     if estado == 'q11':
         if (palavra == 'a'):
+            print("q11 entrou \"" + palavra + "\"" + " vai para q13.")
             return 'q13'
         else:
+            print("q11 entrou \"" + palavra + "\"" + " vai para q10.")
             return 'q10'
 
     if estado == 'q12':
         if (palavra == 'a'):
+            print("q12 entrou \"" + palavra + "\"" + " vai para q14.")
             return 'q14'
         else:
+            print("q12 entrou \"" + palavra + "\"" + " vai para q9.")
             return 'q9'
 
     if estado == 'q13':
         if (palavra == 'a'):
+            print("q13 entrou \"" + palavra + "\"" + " vai para q14.")
             return 'q14'
         else:
+            print("q13 entrou \"" + palavra + "\"" + " vai para q9.")
             return 'q9'
 
     if estado == 'q14':
         if (palavra == 'a'):
+            print("q14 entrou \"" + palavra + "\"" + " vai para q15.")
             return 'q15'
         else:
+            print("q14 entrou \"" + palavra + "\"" + " vai para q11.")
             return 'q11'
 
     if estado == 'q15':
         if (palavra == 'a'):
+            print("q15 entrou \"" + palavra + "\"" + " vai para q15.")
             return 'q15'
         else:
+            print("q15 entrou \"" + palavra + "\"" + " vai para q11.")
             return 'q11'
 
 '''palavra = input("Digite uma palavra do alfabeto (a,b) minusculo")
